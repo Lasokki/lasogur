f0acfe7a4ed4f34d57a6014d90acc6ac01b22061
Erkki Mattila <lasokki@gmail.com>
2015-12-23 00:55:12 +0200

    Naive file uploading implemented

--- lasogur_server.py
+++ lasogur_server.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug import secure_filename
 
-@app.route("/")
-@app.route("/<user>")
-def hello(user=None):
+UPLOAD_FOLDER = 'var/upload'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/<user>", methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
+def upload_file(user=None):
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
     return render_template("index.html", user=user)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
